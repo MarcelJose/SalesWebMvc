768b45a17da1471e85b5283c0b8be064b5d82015
Marcel Sousa <marcelsousa@hotmail.com.br>
2024-11-04 10:38:16 -0300

    Criado MVC Departments

---
+++ SalesWebMvc/Controllers/DepartmentsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> departments = new List<Department>();
+            departments.Add(new Department { Id = 001, Name = "Eletronics"});
+            departments.Add(new Department { Id = 002, Name = "Clothes" });
+            departments.Add(new Department { Id = 003, Name = "Furniture" });
+            departments.Add(new Department { Id = 004, Name = "Office Supplies" });
+            departments.Add(new Department { Id = 005, Name = "Tools" });
+
+            return View(departments);
+        }
+    }
+}
