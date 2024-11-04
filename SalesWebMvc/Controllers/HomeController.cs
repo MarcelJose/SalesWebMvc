9ff7e4dabbc8bd0480d591bf66f45cb6164bb302
Marcel Sousa <marcelsousa@hotmail.com.br>
2024-11-04 09:47:23 -0300

    Criada a View About

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -18,6 +18,14 @@ namespace SalesWebMvc.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Projeto Sales Web MVC do Curso C#";
+            ViewData["Professor"] = "Nelio Alves";
+
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
