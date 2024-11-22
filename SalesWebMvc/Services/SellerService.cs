b09d894b46c714dcda5cd6ff70417fbf83c559c8
Marcel Sousa <marcelsousa@hotmail.com.br>
2024-11-22 12:03:09 -0300

    Adicionado pagina Details para Sellers

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -1,5 +1,6 @@
 ﻿using SalesWebMvc.Data;
 using SalesWebMvc.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace SalesWebMvc.Services
 {
@@ -14,7 +15,7 @@ namespace SalesWebMvc.Services
 
         public List<Seller> FindAll()
         {
-            return _context.Seller.ToList();
+            return _context.Seller.Include(obj => obj.Department).ToList();
         }
 
         public void Insert(Seller seller)
@@ -25,7 +26,7 @@ namespace SalesWebMvc.Services
 
         public Seller FidById(int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
 
         public void Remove(int id)
