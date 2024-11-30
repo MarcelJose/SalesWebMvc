bac7122386615395687d8212f8783ff5bc10d01b
Marcel Sousa <marcelsousa@hotmail.com.br>
2024-11-30 15:20:54 -0300

    Implementado busca agrupada

--- SalesWebMvc/Services/SalesRecordService.cs
+++ SalesWebMvc/Services/SalesRecordService.cs
@@ -25,5 +25,19 @@ namespace SalesWebMvc.Services
                 .OrderByDescending(sr => sr.Date)
                 .ToListAsync();
         }
+
+        public async Task<List<IGrouping<Department,SalesRecord>>> FindByDateGroupingAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from salesRecord in _context.SalesRecord select salesRecord;
+            if (minDate.HasValue) result = result.Where(sr => sr.Date >= minDate.Value);
+            if (maxDate.HasValue) result = result.Where(sr => sr.Date <= maxDate.Value);
+
+            return await result
+                .Include(sr => sr.Seller)
+                .Include(sr => sr.Seller.Department)
+                .OrderByDescending(sr => sr.Date)
+                .GroupBy(sr => sr.Seller.Department)
+                .ToListAsync();
+        }
     }
 }
